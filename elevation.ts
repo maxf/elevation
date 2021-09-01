8359e6d988e5d9f51999aac12a6a78d43e2b8243
Max Froumentin <github@e97.me>
2021-09-01 15:31:48 +0100

    Now using Three.js instead of P5

--- elevation.ts
+++ elevation.ts
@@ -1,77 +1,53 @@
-import p5 from 'p5';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 type Location = {
-  lat: number,
-  lon: number
+  latitude: number,
+  longitude: number
 };
 
-const postBody = function(locs: Array<Location>): string {
-  const locations = locs.map(loc => {
-    return { latitude: loc.lat, longitude: loc.lon };
-  });
-  const json = { locations };
-  return JSON.stringify(json);
-};
 
-const elevationAPiUrl = 'https://api.open-elevation.com/api/v1/lookup';
+const elevationAPiUrl = 'http://localhost:1234/elevations.json';
 
-const getElevations = function(locations: Array<Location>): Promise<any> {
+const getElevations = function(): Promise<any> {
   return new Promise((resolve, reject) =>
-    fetch(
-      elevationAPiUrl,
-      {
-        method: 'POST',
-        body: postBody(locations),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+    fetch(elevationAPiUrl)
       .then(response => response.json())
       .then(jsonResponse => resolve(jsonResponse.results))
       .catch(error => reject(error)));
-}
+};
 
-
-const width: number = 700;
-const height: number = 700;
-const square: number = 20;
-
-const east:number =   2.570290;
-const west:number = -14.931820;
-const north:number = 62.310835;
-const south:number = 48.519515;
-
-const p2g = function(x: number, y: number): {lat: number, lon: number} {
+/*
+const ll2xyz = function({lat: number, lon: number}):{x: number, y: number} {
   return {
     lat: ( y / height) * (south - north) + north,
     lon: ( x / width ) * (east - west) + west
   }
 };
+*/
 
-const sketch = function(p5: p5) {
-  p5.setup = function() {
-    p5.createCanvas(width, height);
-    p5.background(0);
-    p5.fill('blue');
 
-    const locations:Array<Location> = [];
-    for (let x: number = 0; x < width; x += square) {
-      for (let y: number = 0; y < height; y += square) {
-        locations.push(p2g(x, y));
-      }
-    }
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-    getElevations(locations)
-      .then(locationsWithElevations => {
-        for (let x: number = 0; x < width; x += square) {
-          for (let y: number = 0; y < height; y += square) {
-            const elevation = locationsWithElevations.shift().elevation;
-            p5.fill(elevation/3);
-            p5.rect(x, y, square, square);
-          }
-        }
-      })
-  };
-};
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Mesh( geometry, material );
+scene.add( cube );
 
-const myp5 = new p5(sketch);
+camera.position.z = 5;
+
+getElevations()
+  .then(locationsWithElevations => {
+    // create a mesh
+  });
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+animate();
