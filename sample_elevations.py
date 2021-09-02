a39f6224684aa3f80d0359c42a1b66f01564c226
Max Froumentin <github@e97.me>
2021-09-02 21:01:52 +0100

    Specify file name and print information

--- sample_elevations.py
+++ sample_elevations.py
@@ -1,6 +1,15 @@
 import requests
 import json
 import numpy as np
+import sys
+
+
+if len(sys.argv) == 1:
+    print("Usage: %s <output_filename>".format(sys.argv[0]))
+    sys.exit(-1)
+
+output_filename = sys.argv[1]
+
 
 # east =   2.570290
 # west = -14.931820
@@ -13,8 +22,8 @@ west = -180;
 north = 90;
 south = -90
 
-samples_lat = 50
-samples_lon = 50
+samples_lat = 100
+samples_lon = 100
 
 elevation_api_limit = 400
 nb_groups = 1 + samples_lat * samples_lon // elevation_api_limit
@@ -31,10 +40,14 @@ for lat in lats:
 
 sample_groups = np.array_split(samples, nb_groups)
 
+print("We're going to fetch {} elevations from the server using {} http calls".format(len(samples), len(sample_groups)))
 
 response_points = []
-for group in sample_groups:
-    post_payload = {"locations": group.tolist()}
+for i, group in enumerate(sample_groups):
+    locations = group.tolist()
+    post_payload = {"locations": locations}
+    print("Requesting {} elevations ({}/{})".format(
+        len(locations), i+1, len(sample_groups)))
     response = requests.post(
         "https://api.open-elevation.com/api/v1/lookup",
         json=post_payload
@@ -45,9 +58,11 @@ for group in sample_groups:
         response_object = json.loads(response.text)
         response_points += response_object["results"]
 
-
-print(json.dumps({
+print("Writing output to " + sys.argv[1])
+output_file = open(output_filename, "w")
+output_file.write(json.dumps({
     "response": response_points,
     "nLat": samples_lat,
     "nLon": samples_lon
 }))
+print("done.")
