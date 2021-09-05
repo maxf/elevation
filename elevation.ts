46455dc1423ea517b617a29d6b961f43aa71a7ba
Max Froumentin <github@e97.me>
2021-09-05 18:38:21 +0100

    Center on UK and set shadows

--- elevation.ts
+++ elevation.ts
@@ -1,4 +1,4 @@
-import { DirectionalLight, Scene, PerspectiveCamera, WebGLRenderer, BufferAttribute, BufferGeometry, MeshPhongMaterial, Mesh, AmbientLight } from 'three';
+import { PCFSoftShadowMap, DirectionalLight, Scene, PerspectiveCamera, WebGLRenderer, BufferAttribute, BufferGeometry, MeshPhongMaterial, Mesh, AmbientLight } from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 
 
@@ -20,7 +20,7 @@ const getElevations = function(): Promise<any> {
 
 
 const location2vertex = function(l: Location): Array<number> {
-  const r: number = 2 + l.elevation / 20000;
+  const r: number = 2 + l.elevation / 7000;
   const latRadians: number = l.latitude * Math.PI / 180;
   const lonRadians: number = l.longitude * Math.PI / 180;
   return [
@@ -35,11 +35,16 @@ const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const light1 = new DirectionalLight(0xFFFFFF, 1);
 light1.position.set(-1, 2, 4);
+light1.shadow.mapSize.width = 8192;
+light1.shadow.mapSize.height = 8192;
+light1.castShadow = true;
 scene.add(light1);
 const light2 = new AmbientLight(0xFFFFFF, .3);
 scene.add(light2);
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+//renderer.shadowMap.type = PCFSoftShadowMap;
+renderer.shadowMapEnabled = true;
 document.body.appendChild(renderer.domElement);
 const controls = new TrackballControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
@@ -68,7 +73,19 @@ const computeIndices = function(n: number, m: number): Array<number> {
 };
 
 
+// whole world view
 camera.position.z = 5;
+
+
+// centred on UK
+camera.position.x = -0.09195594223398357;
+camera.position.y = 1.8426236013197486;
+camera.position.z = 1.327364264585299;
+camera.rotation.x = -0.9523924208079326;
+camera.rotation.y = -0.04746069682089011;
+camera.rotation.z = -0.06488203924640336;
+
+
 let mesh: Mesh;
 
 getElevations()
@@ -83,6 +100,8 @@ getElevations()
     //const material = new MeshBasicMaterial({ color: 0x00ff00 });
     //material.wireframe = true;
     mesh = new Mesh(geometry, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
     mesh.rotation.x = -Math.PI / 2;
     mesh.rotation.z = 1.5 * Math.PI;
 
@@ -90,7 +109,10 @@ getElevations()
     animate();
   });
 
+let t=0;
 function animate() {
+  t+=0.01;
+  light1.position.x = 20*Math.sin(t);
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
